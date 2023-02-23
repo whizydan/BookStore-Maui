adcf4b8bcbfdfab461c73f2191505bd13e145011
kerberos <whizydan@gmail.com>
2023-02-23 08:37:31 +0300

    added lottie animations

--- Pages/SettingsPage.xaml.cs
+++ Pages/SettingsPage.xaml.cs
@@ -6,4 +6,16 @@ public partial class SettingsPage : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private void light_Clicked(object sender, EventArgs e)
+    {
+        //set theme to light mode
+        Application.Current.UserAppTheme = AppTheme.Light;
+    }
+
+    private void EnterLight(object sender, EventArgs e)
+    {
+        //set theme to dark mode
+        Application.Current.UserAppTheme = AppTheme.Dark;
+    }
 }
